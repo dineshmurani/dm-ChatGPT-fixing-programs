0fa710de771aa1d5588ec2f1908363ad96fed088
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-11-12 10:41:48 -0800

    This program have issue

---
+++ README.md
@@ -0,0 +1,1 @@
+Fixing programs using ChatGPT
---
+++ main.py
@@ -0,0 +1,9 @@
+def rectangle_area(a, b):
+    return a * b
+
+
+a = input("Enter a: ")
+b = input("Enter b: ")
+
+area = rectangle_area(a, b)
+print(area)
